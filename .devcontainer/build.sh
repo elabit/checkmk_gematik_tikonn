cff7fdfd447570b55c1fbec6f5bceb5d49a8e6af
Simon Meggle <simon.meggle@elabit.de>
2022-10-20 16:15:50 +0200

    bar

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,15 +1,66 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: © 2022 ELABIT GmbH <mail@elabit.de>
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-NAME=$(python -c 'print(eval(open("package").read())["name"])')
-rm /omd/sites/cmk/var/check_mk/packages/* ||:
-ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
+# This file creates a CMK MKP file for the project. 
+# It leverages the "mkp" command from CMK, which reads a package description file
+# (JSON).
+# After the MKP has been built, the script check if it runs within a Github 
+# Workflow. If so, it sets the artifact name as output variable.  
+set -e
 
-mkp -v pack $NAME
-
-# Set Outputs for GitHub Workflow steps
-if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "::set-output name=pkgfile::$(ls *.mkp)"
-    echo "::set-output name=pkgname::${NAME}"
-    VERSION=$(python -c 'print(eval(open("package").read())["version"])')
-    echo "::set-output name=pkgversion::$VERSION"
+if [ -z $WORKSPACE ]; then 
+    echo "ERROR: WORKSPACE variable must be set to the project root folder. Exiting."
+    exit 1
 fi
+
+if [ -z $OMD_SITE ]; then 
+    echo "ERROR: You do not seem to be on a OMD site (variable OMD_SITE not set). Exiting."
+    exit 1
+fi 
+
+set -u 
+# set -x 
+# Read the project name
+source $WORKSPACE/project.env
+# checkmk_my_check -> my_check
+CHECK_NAME=${PROJECT_NAME#*_}
+rm /omd/sites/cmk/var/check_mk/packages/* 2>&1 > /dev/null
+PKGFILE=/omd/sites/cmk/var/check_mk/packages/$CHECK_NAME
+cp -f $WORKSPACE/package $PKGFILE.tmp
+PKGNAME=$(jq -r '.name' $PKGFILE.tmp)
+
+echo "Starting MKP build script"
+
+# get the current tag (if Release) or commit hash
+export PKGVERSION=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
+echo "---------------------------------------------"
+echo "▹ Merging package version '$PKGVERSION' into $PKGFILE ..."
+cat $PKGFILE.tmp | jq '. + {version:env.PKGVERSION}' > $PKGFILE
+
+
+echo "---------------------------------------------"
+echo "▹ Building MKP '$PKGNAME' (packagefile: $PKGFILE) ..."
+# set -x
+echo "> mkp -v pack $PKGNAME"
+mkp -v pack $PKGNAME
+
+# get the latest written file
+FILE=$(ls -rt1 *.mkp | tail -1)
+NEWFILENAME=$PKGNAME.$PKGVERSION.mkp
+mv $FILE $NEWFILENAME
+PKG_PATH=$(readlink -f "$NEWFILENAME")
+echo "📦 -> $PKG_PATH"
+echo "---------------------------------------------"
+echo "Checking if we are running within a Github Workflow..."
+if [ -n "${GITHUB_WORKSPACE-}" ]; then
+    echo "🐙 ...Github Workflow exists."
+    echo "▹ Set Outputs for GitHub Workflow steps"
+    echo "::set-output name=pkgfile::$NEWFILENAME"
+    VERSION=$(jq -r '.version' $PKGFILE)
+    echo "::set-output name=artifactname::$NEWFILENAME"
+else 
+    echo "...no GitHub Workflow detected (local execution)."
+fi
+echo "END OF build.sh"
+echo "---------------------------------------------"
---
+++ package.tpl
@@ -0,0 +1,17 @@
+{
+    "title": "",
+    "name": "",
+    "description": "",
+    "author": "Simon Meggle <simon.meggle@elabit.de>",
+    "download_url": "https://github.com/elabit/xxx",
+    "files": {
+        "agent_based": [""],
+        "agents": ["special/"],
+        "checks": [""],
+        "web": [
+            "plugins/wato/",
+            "plugins/wato/"
+        ]
+    },
+    "version.min_required": "2.0.0"
+}
